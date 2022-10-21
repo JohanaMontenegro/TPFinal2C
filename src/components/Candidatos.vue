7948b4a1f940a0ed52d554dbbcbcf88356a3c787
Facundo Mamonde <facundo.mamonde.96@gmail.com>
2022-10-20 22:14:28 -0300

    Home - Inicializacion Candidatos y Empleo

---
+++ src/components/Candidatos.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>Candidatos Test</p>
+</template>
+    
+<script>
+export default {
+  name: 'CandidatosComponent',
+  props: {
+
+  }
+}
+</script>
+    
+<style scoped>
+
+</style>
+    
+
---
+++ src/components/Empleos.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>TEST EMPLEOS</p>
+</template>
+    
+<script>
+export default {
+  name: 'EmpleosComponent',
+  props: {
+
+  }
+}
+</script>
+    
+<style scoped>
+
+</style>
+    
+
+
+
