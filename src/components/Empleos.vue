0b1866f459980f1fb4af3ac28ca0525dfe222389
Facundo Mamonde <facundo.mamonde.96@gmail.com>
2022-10-20 23:45:54 -0300

    Se agrega Empleos

--- src/components/Empleos.vue
+++ src/components/Empleos.vue
@@ -1,20 +1,42 @@
 <template>
-  <p>TEST EMPLEOS</p>
+  <div id="Empleos-container">
+  <EmpleosCard v-for="empleo in empleos" :key="empleo.id" :empleo="empleo"></EmpleosCard>
+</div>
 </template>
-    
+
 <script>
+import EmpleosCard from "./EmpleosCard.vue"
 export default {
   name: 'EmpleosComponent',
   props: {
+    msg: String
+  },
+  components: { EmpleosCard },
 
+  data() {
+    return { empleos:[] }
+  },
+
+  created() {
+    fetch("https://6351b70e9d64d7c71307422e.mockapi.io/empleos/empleos")
+      .then((response) => response.json())
+      .then((trabajo) => {
+        this.empleos = trabajo
+        console.log(this.empleos)
+      });
   }
 }
 </script>
-    
-<style scoped>
+
+<style>
+
+#Empleos-container{
+
+  display:flex;
+  flex-wrap: wrap;
+  padding-top: 30px;
+  padding-left: 30px;
+
+}
 
 </style>
-    
-
-
-
